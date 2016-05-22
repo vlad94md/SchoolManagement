e22733654af8a98260022bd7d16d50ecd91c19c1
Vladislav Guleaev <guleaev.vlad@gmail.com>
2016-05-22 16:57:23 +0300

    Added Secretary functional

--- SchoolManagementSystem/Controllers/SecretaryController.cs
+++ SchoolManagementSystem/Controllers/SecretaryController.cs
@@ -3,16 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SchoolManagementSystem.Domain;
 using SchoolManagementSystem.Models;
 
 namespace SchoolManagementSystem.Controllers
 {
     public class SecretaryController : Controller
     {
-        //
-        // GET: /Secretary/
+        DbSchoolRepository repository = new DbSchoolRepository();
 
         public ActionResult Index()
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            return View();
+        }
+
+
+        public ActionResult Students()
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            var students = repository.Students.ToList();
+
+            return View(students);
+        }
+
+        public ActionResult Teachers()
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            var teachers = repository.Teachers.ToList();
+
+            return View(teachers);
+        }
+
+        public ActionResult AddStudent()
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            return View();
+        }
+
+        public ActionResult AddTeacher()
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            return View();
+        }
+
+        public ActionResult Search(string search)
         {
             var redirector = CheckUserRights();
             if (redirector != null) return redirector;
--- SchoolManagementSystem/Controllers/TeacherController.cs
+++ SchoolManagementSystem/Controllers/TeacherController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -107,13 +108,72 @@ namespace SchoolManagementSystem.Controllers
             return View();
         }
 
-        public ActionResult AddMark(int studentId)
+        public ActionResult AddMark(string studentId)
         {
             var redirector = CheckUserRights();
             if (redirector != null) return redirector;
 
             var currUser = (UserModel)System.Web.HttpContext.Current.Session["user"];
+            var currTeacher = repository.Teachers.FirstOrDefault(x => x.PIN == currUser.Login);
+            var disciplineName = repository.Disciplines.FirstOrDefault(x => x.Id == currTeacher.Discipline_Id);
+            var student = repository.Students.FirstOrDefault(x => x.PIN == studentId);
 
+            if (student != null)
+            {
+                ViewBag.id = studentId;
+                ViewBag.name = student.FirstName + " " + student.LastName;
+                ViewBag.discipline = disciplineName.Subject;
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMark(string id, string date, string mark)
+        {
+            var redirector = CheckUserRights();
+            if (redirector != null) return redirector;
+
+            int month = Int32.Parse(date.Substring(0, 2));
+            int days = Int32.Parse(date.Substring(3, 2));
+            int year = Int32.Parse(date.Substring(6, 4));
+            DateTime actualDate = new DateTime(year, month, days);
+            
+            var currUser = (UserModel)System.Web.HttpContext.Current.Session["user"];
+            var currTeacher = repository.Teachers.FirstOrDefault(x => x.PIN == currUser.Login);
+            var disciplineName = repository.Disciplines.FirstOrDefault(x => x.Id == currTeacher.Discipline_Id);
+            var studnet = repository.Students.FirstOrDefault(x => x.PIN == id);
+
+            if (studnet != null)
+            {
+                ViewBag.id = id;
+                ViewBag.name = studnet.FirstName + " " + studnet.LastName;
+                ViewBag.discipline = disciplineName.Subject;
+            }
+
+            var markExists = repository.Marks.FirstOrDefault(
+                     x => x.Date.Date == actualDate.Date &&
+                     x.Discipline_Id == currTeacher.Discipline_Id &&
+                     x.Student_PIN == id);
+
+            if (markExists != null)
+            {
+                ViewBag.error = "You cant put a mark on the same date and subject twice.";
+                return View();
+            }
+
+            Mark markModel = new Mark()
+            {
+                Value = Int32.Parse(mark),
+                Date = actualDate,
+                Discipline_Id = currTeacher.Discipline_Id,
+                Student_PIN = id,
+                Teacher_PIN = currTeacher.PIN
+            };
+
+            repository.AddMark(markModel);
+
+            ViewBag.success = "Mark was successfully added!";
             return View();
         }
 
