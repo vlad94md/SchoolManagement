d5474fed9ce66c83c22975db3c84a813b3ba474b
vlad94md <i22vladg@mail.ru>
2016-05-23 10:53:20 +0300

    Commit

---
+++ SchoolManagementSystem/Models/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Models
+{
+    public class StudentViewModel
+    {
+        public Student Student { get; set; }
+        public string ClassName { get; set; }
+    }
+}
---
+++ SchoolManagementSystem/Models/TeacherViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SchoolManagementSystem.Domain.Entities;
+
+namespace SchoolManagementSystem.Models
+{
+    public class TeacherViewModel
+    {
+        public Teacher Teacher { get; set; }
+        public string Discipline { get; set; }
+    }
+}
